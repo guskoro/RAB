f82e17596042069291942824e67141bd92f68b65
guskoro <guskoro@gmail.com>
2022-04-28 05:35:00 +0700

    Initial Commit

---
+++ RAB/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RAB
+//
+//  Created by Guskoro Pradipta on 28/04/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RAB/RABApp.swift
@@ -0,0 +1,17 @@
+//
+//  RABApp.swift
+//  RAB
+//
+//  Created by Guskoro Pradipta on 28/04/22.
+//
+
+import SwiftUI
+
+@main
+struct RABApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
